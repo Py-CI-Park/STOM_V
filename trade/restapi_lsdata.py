2fb212e2d4a1f9a36629b8e40295124cb969d759
parkchanil <parkchanil@WORKSTATION.localdomain>
2026-07-12 20:47:39 +0900

    V3U에 V3.35 변경을 pyd-free 방식으로 반영한다
    - V3 formal commit: c6ac10b2 (upstream 경계 9d24b635, 2026-07-07 V3.35 + tail 4건)
    - 반영 범위: c3db5f9c..9d24b635 (12개 파일)
    - pyd 처리: V3.35 범위에 upstream pyd 변경 없음, V3U tracked .pyd 0건 유지
    
    ## V3.35 변경
    
    1. 바이낸스선물 정정주문 추가
    2. LS증권 시장가 주문가격 오류 수정
    3. 주문 예외 처리 강화
    + tail: LS 주문체결 ordxctptncode 누락 방어, 해외선물 체결/정정취소 분리, 수신 처리 간소화
    
    ## V3U 보정
    
    - runtime 보정 0건 (순수 overlay). trade/* 변경은 worker 프로세스 내부, set_dialog_etc는 기존 attr만 참조

--- trade/restapi_lsdata.py
+++ trade/restapi_lsdata.py
@@ -187,7 +187,8 @@ class LsRestData:
         '야간선물주문체결': 'C02',
         '야간선물정정취소': 'H02',
 
-        '해외선물주문체결': 'TC3'
+        '해외선물주문체결': 'TC3',
+        '해외선물정정취소': 'TC2'
     }
 
     tr_data = {
@@ -217,8 +218,8 @@ class LsRestData:
             'body_key': 'CSPAT00601InBlock1',
             'element_keys': ['IsuNo', 'OrdQty', 'OrdPrc', 'BnsTpCode', 'OrdprcPtnCode', 'MgntrnCode', 'LoanDt',
                              'OrdCndiTpCode', 'MbrNo'],
-            'element_values': ['종목코드', '주문수량', '주문가격', '주문구분코드', '호가유형코드', '신용거래코드', '대출일', '주문조건코드',
-                               '회원사번호'],
+            'element_values': ['종목코드', '주문수량', '주문가격', '주문구분코드', '호가유형코드', '신용거래코드', '대출일',
+                               '주문조건코드', '회원사번호'],
             'out_block': 'CSPAT00601OutBlock2'
         },
         '국내주식정정주문': {
